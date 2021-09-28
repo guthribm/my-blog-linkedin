6177349aeb28eacdb21ea66170942f0199fd0aff
Brandon Guthrie <guthribm@gmail.com>
2021-09-28 12:57:59 -0500

    initial commit

---
+++ src/build/precache-manifest.f752b0d581da2d12f0f49211b0064b6c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8dd923fe6821cb064788",
+    "url": "/static/css/main.1a121b82.chunk.css"
+  },
+  {
+    "revision": "8dd923fe6821cb064788",
+    "url": "/static/js/main.bbf6c8e5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ee8f9a6fc57b6b85f315",
+    "url": "/static/js/2.be466273.chunk.js"
+  },
+  {
+    "revision": "f98f70e462d14608ee07120364739399",
+    "url": "/index.html"
+  }
+];
